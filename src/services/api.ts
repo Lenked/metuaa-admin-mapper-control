dda883f5af8c7a38f9d35d61098fe29bbad498d4
Evrard Ntanguen <ntanguenevrard05@gmail.com>
2025-07-25 10:54:52 +0100

    Changement API

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import { PlacesResponse, PlaceFilter } from '@/types/places';
 
 // Nouvelle configuration pour l'API locale
-const API_BASE_URL = 'http://127.0.0.1:8002'; // À modifier facilement plus tard
+const API_BASE_URL = 'http://156.67.104.166:8000'; // À modifier facilement plus tard
 const API_KEY_SESSION = 'b8a7c8e9f0d1a2b3c4d5e6f7a8b9c0d1e2f3a4b5c6d7e8f9a0b1c2d3e4f5a6b7';
 
 const getHeaders = () => ({

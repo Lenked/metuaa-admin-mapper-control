9920d1e946a030bb9e8bf0ea530b5141df81a0be
Paved <pavedngoune@gmail.com>
2025-08-07 20:32:12 +0100

    Config deploy

--- .history/docker-build_20250717164010.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Install any new library if added
-set -e
-
-echo "Build started"
-docker build -t dpaved/web-daddyfoundation:1.0 .
-echo "Build finished successfully"
-docker push dpaved/web-daddyfoundation:1.0
